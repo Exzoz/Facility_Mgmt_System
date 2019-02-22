376e93392f4dd2f8930354e338175f4a10d9e049
Exzoz <farisgjaleel@gmail.com>
2019-02-22 16:17:31 -0600

    updates to Usage

--- src/main/java/com/facility/Usage.java
+++ src/main/java/com/facility/Usage.java
@@ -1,4 +1,39 @@
 package com.facility;
 
+import java.util.Date;
+
+
 public class Usage {
+    Date startDate;
+    Date endDate;
+
+    public Usage() {}
+
+    public Usage(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Usage [startDate=" + startDate + ", endDate=" + endDate + "]";
+    }
 }
+
+
