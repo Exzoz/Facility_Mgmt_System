bf3eafcd113c18acf48314875d5d856c83fb8998
Exzoz <farisgjaleel@gmail.com>
2019-02-21 20:35:25 -0600

    Inspection updates...committing issues via IntelliJ

--- src/main/java/com/facility/Inspection.java
+++ src/main/java/com/facility/Inspection.java
@@ -1,4 +1,28 @@
 package com.facility;
 
+import java.util.Date;
+
 public class Inspection {
+    private Date created;
+    private boolean problemFound;
+
+    public Inspection(Date created, boolean problemFound) {
+        this.created = created;
+        this.problemFound = problemFound;
+    }
+    public Date getCreated() {
+        return created;
+    }
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    public boolean isProblemFound() {
+        return problemFound;
+    }
+    public void setProblemFound(boolean problemFound) {
+        this.problemFound = problemFound;
+    }
+
+
 }
+
