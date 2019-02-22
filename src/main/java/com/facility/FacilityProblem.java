9684cb2018eddaf790079c47bbefc3629685519a
Exzoz <farisgjaleel@gmail.com>
2019-02-21 22:15:55 -0600

    more updates

--- src/main/java/com/facility/FacilityProblem.java
+++ src/main/java/com/facility/FacilityProblem.java
@@ -1,4 +1,16 @@
 package com.facility;
 
+import java.util.Date;
+
 public class FacilityProblem {
-}
+    private Date created;
+    private String description;
+    private boolean solved;
+
+    public FacilityProblem(){}
+
+    public FacilityProblem(Date created, String description, boolean solved) {
+        this.created = created;
+        this.description = description;
+        this.solved = solved;
+    }
--- src/main/java/com/facility/MaintenanceRequest.java
+++ src/main/java/com/facility/MaintenanceRequest.java
@@ -1,4 +1,29 @@
 package com.facility;
 
+import java.util.Date;
+
 public class MaintenanceRequest {
+    private Date created;
+
+    public MaintenanceRequest() {
+        created = new Date();
+    }
+
+    public MaintenanceRequest(Date created) {
+        this.created = created;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "MainteinanceRequest [created=" + created + "]";
+    }
+
 }
