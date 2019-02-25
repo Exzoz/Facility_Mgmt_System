016761da0aa41c90cd63dc6b5cdaef772c082102
Exzoz <farisgjaleel@gmail.com>
2019-02-25 12:41:37 -0600

    saving object serialization

--- src/main/java/com/facility/persistence/SerializeUtils.java
+++ src/main/java/com/facility/persistence/SerializeUtils.java
@@ -1,4 +1,24 @@
 package com.facility.persistence;
 
+import com.facility.model.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class SerializeUtils {
-}
+
+    public static void saveFacility(FacilityObject facilityObject) {
+        try {
+            //save object to file
+            FileOutputStream fileOut = new FileOutputStream("save.db");
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(facilityObject);
+            objectOut.close();
+            System.out.println("Object successfully processed to file");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
