4bf66debe35a613d656a6c103b40ed65485e8e12
Exzoz <farisgjaleel@gmail.com>
2019-04-07 19:01:37 -0400

    minor changes

--- src/main/java/com/facility/domain/model/Inspection.java
+++ src/main/java/com/facility/domain/model/Inspection.java
@@ -1,32 +1,37 @@
 package com.facility.domain.model;
 
-import java.util.Date;
 import java.io.Serializable;
+import java.util.Date;
+
 
 public class Inspection implements Serializable {
     private Date created;
     private boolean problemFound;
 
     public Inspection() {
-
     }
 
     public Inspection(Date created, boolean problemFound) {
         this.created = created;
         this.problemFound = problemFound;
     }
+
     public Date getCreated() {
         return created;
     }
+
     public void setCreated(Date created) {
         this.created = created;
     }
+
     public boolean isProblemFound() {
         return problemFound;
     }
+
     public void setProblemFound(boolean problemFound) {
         this.problemFound = problemFound;
     }
+
     @Override
     public String toString() {
         return "Inspection{" +
@@ -35,6 +40,3 @@ public class Inspection implements Serializable {
                 '}';
     }
 }
-
-
-
